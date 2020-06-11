f48d48a4abdfdfb1ae8c071af940dceecb743e41
Artur Signell <artur@vaadin.com>
2020-06-11 13:30:16 +0300

    TS view

--- frontend/views/ts/ts-view.ts
+++ frontend/views/ts/ts-view.ts
@@ -12,8 +12,19 @@ export class TSView extends LitElement {
 
   render() {
     return html`
-      <br />
-      Content placeholder
+      <vaadin-vertical-layout theme="padding spacing" style="width: 100%;"
+        ><vaadin-text-field tabindex="0" has-label=""></vaadin-text-field
+        ><vaadin-text-field
+          class="fancy"
+          tabindex="0"
+          has-label=""
+        ></vaadin-text-field
+        ><vaadin-button tabindex="0" role="button"
+          >Standard button</vaadin-button
+        ><my-button tabindex="0" role="button">My button</my-button
+        ><fill-indicator></fill-indicator
+        ><vaadin-chart empty-text=" "></vaadin-chart
+      ></vaadin-vertical-layout>
     `;
   }
 }
