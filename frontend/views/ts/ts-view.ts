fb23a554229be65b9a7e15dca79bf8b83581f9e6
Artur Signell <artur@vaadin.com>
2020-09-23 15:28:39 +0300

    Make TS view look the same as Java

--- frontend/views/ts/ts-view.ts
+++ frontend/views/ts/ts-view.ts
@@ -1,4 +1,10 @@
-import { LitElement, html, css, customElement } from "lit-element";
+import "@vaadin/vaadin-button";
+import "@vaadin/vaadin-charts";
+import "@vaadin/vaadin-ordered-layout";
+import "@vaadin/vaadin-text-field";
+import "acme-component/fill-indicator";
+import "acme-component/my-button";
+import { css, customElement, html, LitElement } from "lit-element";
 
 @customElement("ts-view")
 export class TSView extends LitElement {
@@ -13,11 +19,14 @@ export class TSView extends LitElement {
   render() {
     return html`
       <vaadin-vertical-layout theme="padding spacing" style="width: 100%;"
-        ><vaadin-text-field tabindex="0" has-label=""></vaadin-text-field
+        ><vaadin-text-field
+          tabindex="0"
+          label="This is a default text field"
+        ></vaadin-text-field
         ><vaadin-text-field
           class="fancy"
           tabindex="0"
-          has-label=""
+          label="This is a class=fancy text field"
         ></vaadin-text-field
         ><vaadin-button tabindex="0" role="button"
           >Standard button</vaadin-button
